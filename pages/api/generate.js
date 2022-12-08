2f7fe429e6880b5e37513838ed025636542974ee
jacksonfelgenhauer <son@MSI.localdomain>
2022-12-08 19:25:01 +0700

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =  
+`
+Help me write an essay in the style of Michio Kaku and turn it into a tweet thread (without repetitive hashtag) with the topic:  
+
+`
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
